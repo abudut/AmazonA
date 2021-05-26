d1d06460ccb09012d6b2d6642a0014e96bbe2f67
Abudu <abudutouray1999.at@gmail.com>
2021-05-26 22:19:03 +0100

    fc

---
+++ src/components/Header.js
@@ -0,0 +1,75 @@
+import React from "react";
+import Image from "next/image";
+import {
+  MenuIcon,
+  SearchIcon,
+  ShoppingCartIcon,
+} from "@heroicons/react/outline";
+
+function Header() {
+  return (
+    <header>
+      {/*Top NavBar*/}
+      <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
+        <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
+          <Image
+            src="https://links.papareact.com/f90"
+            width={150}
+            height={40}
+            objectFit="contain"
+            className="cursor-pointer"
+          />
+        </div>
+        {/*SearchBar*/}
+        <div className="hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer bg-yellow-400 hover:bg-yellow-500">
+          <input
+            className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4"
+            type="text"
+          />
+          <SearchIcon className="h-12 p-4" />
+        </div>
+
+        {/*RightBar*/}
+        <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
+          <div className="link">
+            <p>Hello Abudu Touray!</p>
+            <p className="font-extrabold md:text-sm ">Account & lists</p>
+          </div>
+          <div className="link">
+            <p>Returs</p>
+            <p className="font-extrabold md:text-sm "> & Orders</p>
+          </div>
+          <div className="relative link flex items-center">
+            <span className="absolute rounded-full right-0 top-0 h-4 w-4 bg-yellow-400 text-center md:right-10 font-bold text-black">
+              0
+            </span>
+            <ShoppingCartIcon className="h-10" />
+            <p className="font-extrabold md:text-sm hidden md:inline mt-2">
+              Basket
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/*Bottom NavBar*/}
+      <div className="flex items-center bg-amazon_blue-light text-white text-sm space-x-3 p-2 pl6">
+        <p className="link flex items-center">
+          <MenuIcon className="h-6 mr-1" />
+          All
+        </p>
+        <p className="link">Prime Video</p>
+        <p className="link">Amazon Buisness</p>
+        <p className="link">Today's Deals</p>
+
+        <p className="link hidden lg:inline">Electronics</p>
+        <p className="link hidden lg:inline">Food & Grocery</p>
+        <p className="link hidden lg:inline">Buy Again</p>
+        <p className="link hidden lg:inline">Shopper Toolkit</p>
+        <p className="link hidden lg:inline">Health & Personal Care</p>
+
+      </div>
+    </header>
+  );
+}
+
+export default Header;
